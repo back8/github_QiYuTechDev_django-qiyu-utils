89692cf59f4708306ab8845a460b0f4bcd3502d8
dev <dev@qiyutech.tech>
2021-01-31 21:13:25 +0800

    export more shared config

--- django_qiyu_utils/settings.py
+++ django_qiyu_utils/settings.py
@@ -7,21 +7,50 @@ from django_qiyu_utils.settings import *
 ```
 """
 
+import os
+
 from .env import EnvHelper
 
 # 只需要导出有用的变量
 #
 # Django 项目导出的配置项
 #
-__all__ = ["DEBUG"]
+__all__ = [
+    "DEBUG",
+    "SECRET_KEY",
+    "ALLOWED_HOSTS",
+    "LANGUAGE_CODE",
+    "TIME_ZONE",
+    "MEDIA_URL",
+    "STATIC_URL",
+]
 
-# 只有设置了 DJANGO_DEV 环境变量,
-# 才   是在 `开发` 环境中运行
-# 否则 是在 `线上` 环境中运行
-# 在开发的时候, 请务必设置 DJANGO_DEV 环境变量
-if EnvHelper.in_prod():  # 先检测线上
-    DEBUG = False
-elif EnvHelper.in_dev() or EnvHelper.in_test():  # 开发 & 测试 环境
+# 安全警告: 请不要在线上环境打开 DEBUG
+DEBUG = False  # 防御性编程 默认不打开 DEBUG
+# 只有设置了 DJANGO_DEV|DJANGO_TEST 环境变量, 才打开 DEBUG
+if EnvHelper.in_dev() or EnvHelper.in_test():
     DEBUG = True
-else:  # 默认 不使用 调试
-    DEBUG = False
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
+#
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.getenv(
+    "DJANGO_SECRET_KEY", "wr*dd-zawlb)xpzw!hi-jbkene_^sb7oqs4_(e7Tz8yklz49_v"
+)
+
+#
+# 允许访问的 HOSTS 列表
+ALLOWED_HOSTS = ["*"]
+# 多个域名使用 ';' 分隔
+hosts = os.getenv("DJANGO_ALLOWED_HOSTS", None)
+if isinstance(hosts, str) and hosts != "":
+    ALLOWED_HOSTS = hosts.split(";")
+##########################################################
+
+LANGUAGE_CODE = "zh-Hans"
+
+TIME_ZONE = "Asia/Shanghai"
+
+MEDIA_URL = "/media/"
+STATIC_URL = "/static/"
