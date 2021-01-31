b5d7759085aa328bacc33c2bce471428877e12bf
dev <dev@qiyutech.tech>
2021-01-31 21:54:47 +0800

    update settings

--- django_qiyu_utils/settings.py
+++ django_qiyu_utils/settings.py
@@ -7,6 +7,7 @@ from django_qiyu_utils.settings import *
 ```
 """
 
+import inspect
 import os
 
 from .env import EnvHelper
@@ -23,6 +24,8 @@ __all__ = [
     "TIME_ZONE",
     "MEDIA_URL",
     "STATIC_URL",
+    "STATIC_ROOT",
+    "STATICFILES_DIRS",
 ]
 
 # 安全警告: 请不要在线上环境打开 DEBUG
@@ -55,3 +58,21 @@ TIME_ZONE = os.getenv("DJANGO_TIME_ZONE", "Asia/Shanghai")
 MEDIA_URL = os.getenv("DJANGO_MEDIA_URL", "/media/")
 
 STATIC_URL = os.getenv("DJANGO_STATIC_URL", "/static/")
+
+#################################################################################
+# this is ugly hack
+frame = inspect.currentframe()
+BASE_DIR = os.path.join(os.path.dirname(inspect.getfile(frame.f_back)), "..", "..")
+
+STATIC_ROOT = None
+STATICFILES_DIRS = None
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "..", "media")
+
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.normpath(os.path.join(BASE_DIR, "..", "static")),
+    ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, "..", "static")
+#################################################################################
